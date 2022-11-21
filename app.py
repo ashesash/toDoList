359b91aa859d78d7b39f4be0929db3fe9308c64d
ashesash <aishwarya.s@live.com>
2022-11-21 17:43:46 +1100

    added form and sqlite

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = 
 
 @app.route('/')
 def index():
