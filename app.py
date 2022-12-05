662f3b432035e9a1c9a76121b52fcf587177ce07
ashesash <aishwarya.s@live.com>
2022-12-05 20:12:48 +1100

    commit

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.app_context().push()
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-db = 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
