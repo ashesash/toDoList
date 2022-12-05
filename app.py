3b9600126210c4f0ae191dac2a5a37d77fe0aa67
ashesash <aishwarya.s@live.com>
2022-12-05 23:54:19 +1100

    list added

--- app.py
+++ app.py
@@ -4,14 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+@app.route("/")
 def index():
-    return render_template('base.html')
+    # show all todos, so we query it
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template("base.html")
+
 
 if __name__ == "__main__":
     db.create_all()
+    new_todo = Todo(title="todo 1", complete=False)
+    db.session.commit()
     app.run(debug=True)
